7d5decf6496dd56d7d1022fc4603464c8a6c5129
Hong Oanh <18521225@gm.uit.edu.vn>
2021-12-16 13:42:41 +0700

    Django Tutorial Part 5: Creating our home page

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,33 @@
 from django.shortcuts import render
+#from django.utils.translation import ugettext as _
+#from django.utils.translation import ugettext_lazy as _
+
 
 # Create your views here.
+from .models import Book, Author, BookInstance, Genre
+
+def index(request):
+    """View function for home page of site."""
+
+    # Generate counts of some of the main objects
+    num_books = Book.objects.count()
+    num_instances = BookInstance.objects.count()
+
+    # Available books (status = 'a')
+    num_instances_available = BookInstance.objects.filter(status__exact='a').count()
+
+    # The 'all()' is implied by default.
+    num_authors = Author.objects.count()
+    #count for genres
+    num_genres = Genre.objects.count()
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+        'num_genres': num_genres,
+        
+    }
+
+    # Render the HTML template index.html with the data in the context variable
+    return render(request, 'index.html', context=context)
