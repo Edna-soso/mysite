88dee78a61e837766736c02a1c58af2ef02f7b8e
Hong Oanh <18521225@gm.uit.edu.vn>
2021-12-27 11:40:14 +0700

    Django Tutorial Part 6: Generic list and detail views

--- catalog/models.py
+++ catalog/models.py
@@ -15,7 +15,6 @@ class Book(models.Model):
     # Foreign Key used because book can only have one author, but authors can have multiple books
     # Author as a string rather than object because it hasn't been declared yet in the file
     author = models.ForeignKey('Author', on_delete=models.SET_NULL, null=True)
-
     summary = models.TextField(max_length=1000, help_text='Enter a brief description of the book')
     isbn = models.CharField('ISBN', max_length=13, unique=True,
                              help_text='13 Character <a href="https://www.isbn-international.org/content/what-isbn">ISBN number</a>')
@@ -67,14 +66,11 @@ class Author(models.Model):
     last_name = models.CharField(max_length=100)
     date_of_birth = models.DateField(null=True, blank=True)
     date_of_death = models.DateField('Died', null=True, blank=True)
-
-    class Meta:
-        ordering = ['last_name', 'first_name']
-
     def get_absolute_url(self):
-        """Returns the url to access a particular author instance."""
         return reverse('author-detail', args=[str(self.id)])
 
     def __str__(self):
-        """String for representing the Model object."""
         return f'{self.last_name}, {self.first_name}'
+
+    class Meta:
+        ordering = ['last_name', 'first_name']
--- catalog/views.py
+++ catalog/views.py
@@ -1,15 +1,9 @@
 from django.shortcuts import render
-#from django.utils.translation import ugettext as _
-#from django.utils.translation import ugettext_lazy as _
-
-
-# Create your views here.
+from django.views import generic
 from .models import Book, Author, BookInstance, Genre
+from django.http import Http404
 
 def index(request):
-    """View function for home page of site."""
-
-    # Generate counts of some of the main objects
     num_books = Book.objects.count()
     num_instances = BookInstance.objects.count()
 
@@ -31,3 +25,36 @@ def index(request):
 
     # Render the HTML template index.html with the data in the context variable
     return render(request, 'index.html', context=context)
+class BookListView(generic.ListView):
+    model = Book
+    paginate_by = 2
+    def get_context_data(self, **kwargs):
+        context = super(BookListView, self).get_context_data(**kwargs)
+        context['some_data'] = 'This is just some data'
+        return context
+
+class BookDetailView(generic.DetailView):
+    model = Book
+    def book_detail_view(request, primary_key):
+        try:
+            book = Book.objects.get(pk=primary_key)
+        except Book.DoesNotExist:
+            raise Http404('Book does not exist')
+        return render(request, 'catalog/book_detail.html', context={'book': book})
+
+class AuthorListView(generic.ListView):
+    model = Author
+    paginate_by = 2
+    def get_context_data(self, **kwargs):
+        context = super(AuthorListView, self).get_context_data(**kwargs)
+        return context
+
+class AuthorDetailView(generic.DetailView):
+    model = Author
+    def author_detail_view(request, primary_key):
+        try:
+            author = Author.objects.get(pk=primary_key)
+        except Author.DoesNotExist:
+            raise Http404('Author does not exist')
+        return render(request, 'catalog/author_detail.html', context={'author': author})
+       
